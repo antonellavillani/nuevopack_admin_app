28ee295120c2bfd166077d37349f2ef6b7d7d5b8
Lara Antonella Villani Magrini <lara.villani@davinci.edu.ar>
2025-07-01 15:24:55 -0300

    feat: Agregué pantalla de login con layout inicial

--- app/src/main/java/com/nuevopack/admin/MainActivity.java
+++ app/src/main/java/com/nuevopack/admin/MainActivity.java
@@ -2,11 +2,35 @@ package com.nuevopack.admin;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.text.InputType;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Pantalla Login
+        EditText inputPassword = findViewById(R.id.inputPassword);
+        ImageView eyeToggle = findViewById(R.id.eyeToggle);
+
+        eyeToggle.setOnClickListener(v -> {
+            if (inputPassword.getInputType() == (InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD)) {
+                inputPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                eyeToggle.setImageResource(R.drawable.ic_hide_password); // ícono "ocultar"
+                inputPassword.setTypeface(ResourcesCompat.getFont(this, R.font.roboto_regular));
+
+            } else {
+                inputPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                eyeToggle.setImageResource(R.drawable.ic_show_password); // ícono "ver"
+                inputPassword.setTypeface(ResourcesCompat.getFont(this, R.font.roboto_regular));
+            }
+            inputPassword.setSelection(inputPassword.getText().length());
+        });
     }
 }
